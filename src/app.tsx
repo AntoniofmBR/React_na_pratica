1d76a57179ea6a8925408fe1abbd25d5635c7b2c
Antonio Fonseca <antonio.fonseca@4netsolutions.com.br>
2024-02-26 17:35:26 -0300

    create create-tag-form

--- src/app.tsx
+++ src/app.tsx
@@ -7,8 +7,9 @@ import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '.
 import { Pagination } from './components/pagination';
 import { keepPreviousData, useQuery } from '@tanstack/react-query';
 import { useSearchParams } from 'react-router-dom';
-import { useState } from 'react';
-// import useDebounceValue from './hooks/use-debouce-value';
+import { useState, FormEvent } from 'react';
+import * as Dialog from '@radix-ui/react-dialog';
+import { CreateTagForm } from './components/create-tag-form'
 
 export interface TagResponse {
   first: number
@@ -71,10 +72,30 @@ export function App() {
         <h1 className='text-xl font-bold'>
           Tags
         </h1>
-        <Button variant='primary'>
-          <Plus className='size-3' />
-          Create new
-        </Button>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <Button variant='primary'>
+              <Plus className='size-3' />
+              Create new
+            </Button>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+          <Dialog.Overlay className='fixed inset-0 bg-black/78'/>
+          <Dialog.Content className='fixed space-y-10 p-10 right-0 top-0 bottom-0 h-screen min-w-[320px] z-10 bg-zinc-950 border-l border-zinc-400'>
+          <div className='space-y-3'>
+          <Dialog.Title className='text-xl font-bold'>
+            Create tag
+          </Dialog.Title>
+          <Dialog.Description className='text-sm text-zinc-500'>
+            Tags can be used to group videos about similar concepts
+          </Dialog.Description>
+          </div>
+          <CreateTagForm />
+          <Dialog.Close />
+          </Dialog.Content>
+         </Dialog.Portal>  
+        </Dialog.Root>
       </div>
 
       <div className='flex items-center justify-between'>
